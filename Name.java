8dd0cacc3ce8cb8393319020d61481ab0e7be74a
amandatorresapu <69279617+amandatorresapu@users.noreply.github.com>
2022-04-02 10:22:32 -0700

    first commit

---
+++ Name.java
@@ -0,0 +1,7 @@
+public class Name{
+    public static void main(String[] args){
+        System.out.println("My name is Amanda Torres");
+        System.out.println("I am too years old");
+        System.out.println("My hometown is Cypress California");
+    }
+}
